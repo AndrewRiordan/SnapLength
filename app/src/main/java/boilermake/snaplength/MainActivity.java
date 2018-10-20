fbd40d68dab50ac2338dcda63998075ae7ec9259
AndrewThomasRiordan <riordana@purdue.edu>
2018-10-20 10:37:18 -0400

    Fixed Dependancies

--- app/src/main/java/boilermake/snaplength/MainActivity.java
+++ app/src/main/java/boilermake/snaplength/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     // Capture and Save Files
     // Release the Camera
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
