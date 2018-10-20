2cbad601ac35a9c3039aaf9c336e32d091e7389d
VivekNair360 <nair42@purdue.edu>
2018-10-20 00:16:06 -0400

    Vivek's camera work part 1

--- app/src/main/java/boilermake/snaplength/MainActivity.java
+++ app/src/main/java/boilermake/snaplength/MainActivity.java
@@ -1,13 +1,47 @@
 package boilermake.snaplength;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // Detect and Access Camera
+    // Create a Preview Class
+    // Build a Preview Layout
+    // Setup Listeners for Capture
+    // Capture and Save Files
+    // Release the Camera
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Check whether or not device is equipped w camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+    /** Gets instance of Camera object */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open(); // tries to get Camera instance
+        }
+        catch (Exception e) {
+            // Camera not available for whatever reason
+        }
+        return c; //return null if camera is unavailable
+    }
+
+
 }
